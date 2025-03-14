71ca77745ee1542a88a04d4511e94b7178e06739
Samuel Florez Villaquiran <1110366605@u.icesi.edu.co>
2025-03-14 18:00:08 -0500

    se aprendio javascript

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,7 @@
 
 /*
 Esto es un gran comentario!
-*/
+
 
 
 // ------------------------ 2. variables
@@ -16,12 +16,27 @@ variableLet = "Mundo!"
 
 variableLet = "Esto es una variable"
 
-console.log(variableLet)
+//console.log(variableLet)
 
 //Constante (Sirva para datos que se repiten varias veces)
  
 const constanteConts = "Eso es una constante"
 
-console.log(ConstanteConts)
+///console.log(ConstanteConts)
 
 var Esmayor = true
+
+*/
+
+const formulario = document.getElementById('formulario')
+
+formulario.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const nombre = document.getElementById('nombre').value
+    const edad = document.getElementById("number").value
+    const fechadenacimiento = document.getElementById("fechadenacimiento").value
+
+    const textoFinal = `Mi nombre es ${nombre}, mi edad es ${edad} y naci el ${fechadenacimiento}`
+    document.getElementById('resultado').textContent = textoFinal
+})
