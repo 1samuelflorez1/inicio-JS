b4daf91c3840ed3c1777fa91ab671a42647ffa23
Samuel Florez Villaquiran <1110366605@u.icesi.edu.co>
2025-03-14 17:21:03 -0500

    carpeta de JS

---
+++ scripts/app.js
@@ -0,0 +1,27 @@
+
+//------------------1. comentarios
+
+/*
+Esto es un gran comentario!
+*/
+
+
+// ------------------------ 2. variables
+
+//variable (puede cambiar su valor)
+
+let variableLet = "Hola" 
+
+variableLet = "Mundo!"
+
+variableLet = "Esto es una variable"
+
+console.log(variableLet)
+
+//Constante (Sirva para datos que se repiten varias veces)
+ 
+const constanteConts = "Eso es una constante"
+
+console.log(ConstanteConts)
+
+var Esmayor = true
